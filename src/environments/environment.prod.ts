d8492db5e7350c4781f49ac81fbbd898f4db15b8
frontdesk-beep <frontdesk@careinsurance.ca>
2026-08-26 01:43:38 -0400

    live url chnaged.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://your-app-name.azurewebsites.net/api',
-    hubUrl: 'https://your-app-name.azurewebsites.net/taskHub'
+  production: true,
+  apiUrl: 'https://task-manager-care-api-cybke6eqhjf9g9dn.westcentralus-01.azurewebsites.net/api',
+  hubUrl: 'https://task-manager-care-api-cybke6eqhjf9g9dn.westcentralus-01.azurewebsites.net/taskHub'
 };
